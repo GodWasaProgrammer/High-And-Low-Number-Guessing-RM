f44dd1ba5f6d7305e2b661ebb826b81ec41436cf
GodWasaProgrammer <bwnils@gmail.com>
2022-12-16 22:13:27 +0100

    Complete Restart.

--- Program.cs
+++ Program.cs
@@ -5,48 +5,6 @@ namespace High_And_Low_Number_Guessing_RM
     {
         public static void Main()
         {
-            // instantiated the game logic
-            var go = new HighLowRandom();
-            // loop controller 
-            // declared true until game is done
-            bool loop = true;
-            int chances = 0;
-            // sets winner as the secret number
-            // calls our welcomemessage method
-            go.WelcomeMessage();
-            // do while loop to loop our game until we done
-            do
-            {
-                // sets the returned value from takeinput to our local int variable input to be evaluated against the instantiated mysecretnumber
-                Chances();
-                int input = InputHandler.TakeInput();
-                go.CalculateGuess(input);
-                if (InputHandler._ParseState == true)
-                { 
-                chances++;
-                }
-            }
-            // keeps loop going while loop equals true
-            while (loop == true);
-            // chance method
-             void Chances()
-            {
-                if (chances < 5)
-                {
-                    Console.WriteLine($"Your Current amount of chances spent:{chances} out of 5");
-                }
-                if (chances == 5)
-                {
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"Your Current amount of chances spent:{chances} out of 5 ");
-                    Console.WriteLine("You have lost!");
-                    chances = 0;
-                    go.NewSecretNumber();
-                    go.WelcomeMessage();
-                }
-            }
-            // prints the winning number
-            Console.WriteLine($"You won with:{go.MySecretNumber}");
-        }
-    }
+           
+
 }
