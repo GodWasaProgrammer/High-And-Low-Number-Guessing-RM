071bf6df76fb3c21d5e4c19690933284e2609d1f
GodWasaProgrammer <bwnils@gmail.com>
2022-12-18 19:00:57 +0100

    Corrected my PascalCase variables to the correct, camelCase

--- Program.cs
+++ Program.cs
@@ -6,83 +6,83 @@ namespace High_And_Low_Number_Guessing_RM
         public static void Main()
         {
             // Secret number
-            var MySecretNumber = new Random().Next(0, 100);
+            int mySecretNumber = new Random().Next(0, 100);
             // our maximum value
-            int MaxValue = MySecretNumber + 5;
+            int maxValue = mySecretNumber + 5;
             // our minimum value
-            int MinValue = MySecretNumber - 5;
+            int minValue = mySecretNumber - 5;
             // our guess as string
-            string MyGuess;
+            string myGuess;
             // our parsebool
-            bool IsParsable;
+            bool isParsable;
             // our guess when parsed to an integer
-            int MyGuessasInt;
+            int myGuessasInt;
             // our chance integer
-            int Chances = 5;
+            int chances = 5;
             // chance do while loop, do while chances are not 0
             do
             {
-                if (Chances == 0)
+                if (chances == 0)
                 {
-                    Chances = 5;
+                    chances = 5;
                 }
                 // Chance loop
                 do
                 {
                     Console.BackgroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"Your Current amount of chances: {Chances}");
-                    Chances--;
+                    Console.WriteLine($"Your Current amount of chances: {chances}");
+                    chances--;
                     // Parse Loop
                     do
                     {
                         Console.WriteLine("Write your guess");
-                        MyGuess = Console.ReadLine();
-                        Console.WriteLine(MyGuess);
-                        IsParsable = Int32.TryParse(MyGuess, out MyGuessasInt);
-                        Console.WriteLine("our max value is: " + MaxValue);
-                        Console.WriteLine("our min value is : " + MinValue);
+                        myGuess = Console.ReadLine();
+                        Console.WriteLine(myGuess);
+                        isParsable = Int32.TryParse(myGuess, out myGuessasInt);
+                        Console.WriteLine("our max value is: " + maxValue);
+                        Console.WriteLine("our min value is : " + minValue);
                         // if my guess is below the max value AND above the secret number, do this
-                        if (MyGuessasInt < MaxValue && MyGuessasInt > MySecretNumber)
+                        if (myGuessasInt < maxValue && myGuessasInt > mySecretNumber)
                         {
                             Console.WriteLine("Too high!");
                             Console.WriteLine("You were Above the secret number, within 5 of the target");
                             // if the number is more then 5 off
-                            if (MyGuessasInt > MaxValue)
+                            if (myGuessasInt > maxValue)
                             {
                                 Console.WriteLine("Way off... too high.");
                             }
                         }
                         // if my guess is below the minvalue AND below mysecretnumber
-                        if (MyGuessasInt > MinValue && MyGuessasInt < MySecretNumber)
+                        if (myGuessasInt > minValue && myGuessasInt < mySecretNumber)
                         {
                             Console.WriteLine("Too low!");
                             Console.WriteLine("You were Below the secret number, within 5 of the target");
                             // if the number is more then 5 off
-                            if (MyGuessasInt < MinValue)
+                            if (myGuessasInt < minValue)
                             {
                                 Console.WriteLine("Way off..too low!");
                             }
                         }
                         // if you run out of chances
-                        if (Chances == 0)
+                        if (chances == 0)
                         {
                             Console.BackgroundColor = ConsoleColor.DarkRed;
                             Console.WriteLine("You Lose!");
                             // Kills the Game
-                            Chances = -1;
+                            chances = -1;
                         }
                         // if you win
-                        if (MyGuessasInt == MySecretNumber)
+                        if (myGuessasInt == mySecretNumber)
                         {
                             Console.WriteLine("Amazing. You Win The Prize!111");
                             Console.WriteLine("Game restarting.");
                             // sets chances to 0, And restarts the game
-                            Chances = 0;
+                            chances = 0;
                         }
                     }
-                    while (IsParsable == false);
-                } while (Chances > 0);
-            } while (Chances > -1);
+                    while (isParsable == false);
+                } while (chances > 0);
+            } while (chances > -1);
         }
     }
 }
